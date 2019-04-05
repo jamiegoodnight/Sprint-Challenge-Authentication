8a9d7a8cd1e823a117deca9251e9854ba736b266
Jamie Goodnight <goodnightlambda@gmail.com>
2019-04-05 11:42:46 -0400

    Composed login function and generateToken function

--- config/routes.js
+++ config/routes.js
@@ -36,7 +36,35 @@ function register(req, res) {
 function login(req, res) {
   const { username, password } = req.body;
 
-  if(username && )
+  if (username && password) {
+    db.getUserByName({ username })
+      .then(user => {
+        if (bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({ message: "LOGGED IN", token });
+        } else {
+          res.status(401).json({ message: "Invalid username or password!" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "There was a problem logging in!" });
+      });
+  } else {
+    res
+      .status(401)
+      .json({ message: "Please provide a username and password to login!" });
+  }
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secret, options);
 }
 
 function getJokes(req, res) {
