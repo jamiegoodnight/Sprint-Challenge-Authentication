606bce08a9db9048361595348010748c767ce543
Jamie Goodnight <goodnightlambda@gmail.com>
2019-04-05 11:28:35 -0400

    Initial commit! Added a routersModels file. Working on routes. Register function composed.

--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,55 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const { authenticate } = require('../auth/authenticate');
+const db = require("./routesModels");
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  if (user.username && user.password) {
+    db.addUser(user)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: "There was a problem saving your user!" });
+      });
+  } else {
+    res
+      .status(401)
+      .json({ message: "Please provide a username and password!" });
+  }
 }
 
 function login(req, res) {
-  // implement user login
+  const { username, password } = req.body;
+
+  if(username && )
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
